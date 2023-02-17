127987356163f1426539eb9d3e7879b7e47b5628
Ross Voerman <voermanr@oregonstate.edu>
2023-02-17 14:43:49 -0800

    Added tests

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "myalloc.h"
 #include <stdlib.h>
+#include <assert.h>
 
 int main(void) {
 
@@ -7,7 +8,17 @@ int main(void) {
     void *p = NULL;
     myfree(p);
 
-    myalloc(1);
+    //returns non null ptr
+    assert(myalloc(1));
+
+    //
+    {
+        block *myalloc_return = myalloc(1);
+
+        assert(myalloc_return->in_use == 0);
+        assert(myalloc_return->next == NULL);
+        assert(myalloc_return->size > 0);
+    }
 
     return 0;
 }
